5c655beef0f29a26641f677cec3977284795e98f
steeksg <53086378+steeksg@users.noreply.github.com>
2022-03-28 20:24:02 +0300

    Edit readme and slyled fix

--- src/components/header/buttons/Buttons.tsx
+++ src/components/header/buttons/Buttons.tsx
@@ -1,13 +1,15 @@
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 
-import ButtonSignIn from "./signin/ButtonSignIn"
-import ButtonSignUp from "./signup/ButtonSignUp"
+import ButtonSignIn from "./signin/ButtonSignIn";
+import ButtonSignUp from "./signup/ButtonSignUp";
 
-import "./Buttons.scss"
+import "./Buttons.scss";
 
 export default function Buttons() {
-    return <Stack spacing={2} direction="row">
-            <ButtonSignIn />
-            <ButtonSignUp />
+  return (
+    <Stack className="buttons--wrap" spacing={2} direction="row">
+      <ButtonSignIn />
+      <ButtonSignUp />
     </Stack>
-};
+  );
+}
