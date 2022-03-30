905317c1c30bc9b96914646906ad4455e0f65fe8
steeksg <53086378+steeksg@users.noreply.github.com>
2022-03-31 01:59:15 +0300

    #13 Redirect withiut signup

--- src/components/body/pages/history/PageHistory.tsx
+++ src/components/body/pages/history/PageHistory.tsx
@@ -10,6 +10,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   getHistoryByUser,
+  getUsernameFromLS,
   resetHistoryByUsername,
 } from "../../../../utils/localStorage";
 
@@ -18,6 +19,12 @@ import "./PageHistory.scss";
 export default function PageHistory() {
   const history = getHistoryByUser();
   let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!getUsernameFromLS()) {
+      navigate("/signin");
+    }
+  }, []);
 
   return (
     <React.Fragment>
@@ -33,40 +40,41 @@ export default function PageHistory() {
         </Button>
       </div>
       <List className="pageHistory--wrap">
-        {history.map((record, index) => {
-          return (
-            <React.Fragment key={record.name + index}>
-              <ListItem
-                className="pageHistory--item"
-                alignItems="flex-start"
-                onClick={() => {
-                  navigate(
-                    `/search?name=${record.name}&species=${record.species}&gender=${record.gender}&status=${record.status}&page=1`
-                  );
-                }}
-              >
-                <ListItemText
-                  className="pageHistory--title"
-                  primary={`Name: ${record.name || "Empty"}`}
-                  secondary={
-                    <React.Fragment>
-                      <Typography className="pageHistory--filter">
-                        {`Gender: ${record.gender || "Empty"}`}
-                      </Typography>
-                      <Typography className="pageHistory--filter">
-                        {`Species: ${record.species || "Empty"}`}
-                      </Typography>
-                      <Typography className="pageHistory--filter">
-                        {`Status: ${record.status || "Empty"}`}
-                      </Typography>
-                    </React.Fragment>
-                  }
-                />
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </React.Fragment>
-          );
-        })}
+        {history &&
+          history.map((record, index) => {
+            return (
+              <React.Fragment key={record.name + index}>
+                <ListItem
+                  className="pageHistory--item"
+                  alignItems="flex-start"
+                  onClick={() => {
+                    navigate(
+                      `/search?name=${record.name}&species=${record.species}&gender=${record.gender}&status=${record.status}&page=1`
+                    );
+                  }}
+                >
+                  <ListItemText
+                    className="pageHistory--title"
+                    primary={`Name: ${record.name || "Empty"}`}
+                    secondary={
+                      <React.Fragment>
+                        <Typography className="pageHistory--filter">
+                          {`Gender: ${record.gender || "Empty"}`}
+                        </Typography>
+                        <Typography className="pageHistory--filter">
+                          {`Species: ${record.species || "Empty"}`}
+                        </Typography>
+                        <Typography className="pageHistory--filter">
+                          {`Status: ${record.status || "Empty"}`}
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+                <Divider variant="inset" component="li" />
+              </React.Fragment>
+            );
+          })}
       </List>
     </React.Fragment>
   );
